6e5c70b8d323c6f67f8fac05fb942d8e25a6b0d6
Brian Bird <birdb@lanecc.edu>
2016-01-26 11:14:21 -0800

    Code from last week

---
+++ Demos/BookStoreDemo1/BookStoreDemo1/Models/BookViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreDemo1.Models
+{
+    public class BookViewModel
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public string ISBN { get; set; }
+        public Stack StackItem { get; set; }
+    }
+}
