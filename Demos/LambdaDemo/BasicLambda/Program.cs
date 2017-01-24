5eaa04445ee08263e84cbc777e65d99feb64b044
Brian Bird <profbird@gmail.com>
2017-01-24 08:37:02 -0800

    Added a Lambda demo
    Shows using Lambda expressions for method definition, anonymous
    methods, methods as parameters, and read-only properties

---
+++ Demos/LambdaDemo/BasicLambda/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BasicLambda
+{
+	class MathOps
+	{
+		private int number1;
+		private int number2;
+
+		// Normal read-only property
+		public int Number1 { get { return number1; } }
+
+		// Lambda read-only properties
+		public int Number2 => number2;
+		public int Quotient => number1 / number2;
+
+		// Constructors
+		public MathOps() { }
+
+		public MathOps(int n1, int n2)
+		{
+			number1 = n1;
+			number2 = n2;
+		}
+		               
+		// A normal method definition
+		public static int Sum(int a, int b)
+		{
+			return a + b;
+		}
+
+		// A lambda method difinition
+		public static int Product(int a, int b) => a * b;
+
+		// This method accepts another method as an argument
+		public static void DisplayResult(Func<int, int, int> MathOp, int num1, int num2)
+		{
+			Console.WriteLine(MathOp(num1, num2));
+		}
+	}
+
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int a = 3, b = 7;
+			Console.WriteLine("MathOp using Sum");
+			MathOps.DisplayResult(MathOps.Sum, a, b);
+
+			Console.WriteLine("MathOp using Product");
+			MathOps.DisplayResult(MathOps.Product, a, b);
+
+			Console.WriteLine("MathOp using an anonymous method");
+			MathOps.DisplayResult((n1, n2) => n1 - n2, a, b);
+
+			Console.WriteLine("Using MathOps properties");
+			var math = new MathOps(110, 11);
+			Console.WriteLine(String.Format("{0} / {1} = {2}",
+			               math.Number1, math.Number2, math.Quotient));
+		}
+	}
+}
---
+++ Demos/LambdaDemo/LambdaDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace LambdaDemo
+{
+	class Book
+	{
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public int Year { get; set; }
+	}
+
+	class Program
+	{
+		// Can get a book by using the search method which is passed in as an argument
+		static Book GetBook(string searchParam, List<Book> books, Func<string, List<Book>, Book> search)
+		{
+			return search(searchParam, books);
+		}
+
+		static void Main(string[] args)
+		{
+			List<Book> books = new List<Book>  {
+			new Book() {Title = "Book1", Author = "Auth1", Year = 2013},
+			new Book() {Title = "Book2", Author = "Auth2", Year = 2010},
+			new Book() {Title = "Book3", Author = "Auth3", Year = 1900},
+			};
+
+			Book bookFound = GetBook("Book2", books,
+				(title, bookList) =>
+				{
+					foreach (Book b in books)
+						if (b.Title == title)
+							return b;
+					return null;
+				});
+		}
+	}
+}
