f0e45a3421537f103401178f2e4f05c5dfe440a1
Student <Student>
2017-01-24 09:56:34 -0800

    Added a demo from class

---
+++ Demos/FirstCoreMVCApp/src/FirstCoreMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,40 @@
+﻿using FirstCoreMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstCoreMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        public IActionResult ShowMovies()
+        {
+            var movie1 = new Movie();
+            movie1.Title = "Armageddon";
+            movie1.ReleaseDate = new System.DateTime(1998, 7, 1);
+            movie1.Actors = new List<string>() {"Bruce Willis",
+            "Ben Afflic", "Steve Bucemi", "Liv Taylor"};
+
+            return View(movie1);
+        }
+    }
+}
---
+++ Demos/FirstCoreMVCApp/src/FirstCoreMVCApp/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstCoreMVCApp.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public List<string> Actors { get; set; }
+    }
+}
