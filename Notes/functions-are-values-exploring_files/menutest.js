73fc1fac6b26f1d9d687cae47b5d30fa9b6d3e8a
Brian Bird <birdb@lanecc.edu>
2019-01-06 09:22:03 -0800

    Moved notes to OneDrive

--- Notes/functions-are-values-exploring_files/menutest.js
+++
@@ -1,1 +0,0 @@
-function expand(s){  var td = s;  var d = td.getElementsByTagName("div").item(0);  td.className = "menuHover";  d.className = "menuHover";}function collapse(s){  var td = s;  var d = td.getElementsByTagName("div").item(0);  td.className = "menuNormal";  d.className = "menuNormal";}
